376112f0d0e3e9a789ddd1f6ef96cd70c1b6f04a
axelsundin <axelsundin@hotmail.com>
2021-01-16 13:29:44 +0100

    #1
    added php files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naturresservatet</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" placeholder="APA">
+        <input type="text" placeholder="GIRAFF">
+        <input type="text" placeholder="TIGER">
+        <input type="text" placeholder="KOKOSNÖT">
+
+        <button type="submit">SPARA</button>
+    </form>
+</body>
+</html>
